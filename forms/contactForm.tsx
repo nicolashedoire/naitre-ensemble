07ec5a28daf0174a7120fbd166bb6376936e9f26
Nicolas Hedoire <nicolashedoire@hotmail.com>
2021-04-06 02:36:31 +0200

    fix form with material-ui input

--- forms/contactForm.tsx
+++ forms/contactForm.tsx
@@ -1,27 +1,37 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import styles from './styles.module.scss';
 
 type Inputs = {
   email: string,
   message: string,
 };
 
-export default function Contact() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    }
+  }));
 
+export default function Contact() {
+    
+    const classes = useStyles();
     const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
     const onSubmit = data => console.log(data);
 
     return (
       <div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="" {...register("email", { required: true })} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("message", { required: true })} />
-      {/* errors will return when field validation fails  */}
+    <form className={`${classes.root} ${styles.formGrid}`} onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+      <TextField id="form-email" label="Email" defaultValue="" {...register("email", { required: true })}/>
+      <TextField id="form-message" label="Message" defaultValue="" {...register("message", { required: true })}/>
       {errors.message && <span>Le champ message est requis</span>}
-      <input type="submit" />
+      <Button type="submit" variant="outlined" color="default" disableElevation>Prendre contact</Button>
     </form>
       </div>
     );
